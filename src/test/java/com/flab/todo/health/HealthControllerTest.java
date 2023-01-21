3c15eb9fa6f59600d5bb6f4f3c52c1de8e6a7cab
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-01-21 16:23:23 +0900

    feat : Refactor our code

--- src/test/java/com/flab/todo/health/HealthControllerTest.java
+++ src/test/java/com/flab/todo/health/HealthControllerTest.java
@@ -6,12 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class HealthControllerTest {
+class HealthControllerTest{
 
 	@Autowired
 	TestRestTemplate testRestTemplate;
@@ -28,7 +27,7 @@ class HealthControllerTest {
 			);
 
 		//Then
-		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(HttpStatus.OK,response.getStatusCode());
 		assertEquals("pong",response.getBody());
 	}
 }
