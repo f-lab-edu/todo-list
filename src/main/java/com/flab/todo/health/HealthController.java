c16e6d4659932ab4ab47952da87b0d8b9997eb60
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-01-21 16:11:16 +0900

    feat : Create Health Controller And Test

---
+++ src/main/java/com/flab/todo/health/HealthController.java
@@ -0,0 +1,12 @@
+package com.flab.todo.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+	@GetMapping("/ping")
+	public String pong(){
+		return "pong";
+	}
+}
