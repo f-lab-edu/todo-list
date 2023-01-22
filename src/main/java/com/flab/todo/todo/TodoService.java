6936651abc71487e9d43bae4ff37ea523a613381
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-01-23 03:00:24 +0900

    feat : Create Todo Service & Test

---
+++ src/main/java/com/flab/todo/todo/TodoService.java
@@ -0,0 +1,24 @@
+package com.flab.todo.todo;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.flab.todo.common.dto.RequestTodoListDto;
+
+@Service
+public class TodoService {
+
+	private final TodoRepository todoRepository;
+
+	public TodoService(TodoRepository todoRepository) {
+		this.todoRepository = todoRepository;
+	}
+
+	public List<RequestTodoListDto> getTodoList(Long userId){
+		return this.todoRepository.findByUserId(userId).stream()
+			.map(x -> x.toRequestTodoListDto())
+			.collect(Collectors.toList());
+	}
+}
