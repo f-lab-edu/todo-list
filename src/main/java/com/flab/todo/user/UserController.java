810004013a689d0e3019e01fdf2a6b45f486d232
Jaeyeon Cho <cjyeon1022@gmail.com>
2023-01-24 23:39:52 +0900

    Update UserController.java

--- src/main/java/com/flab/todo/user/UserController.java
+++ src/main/java/com/flab/todo/user/UserController.java
@@ -4,6 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+<<<<<<< Updated upstream
+=======
+import org.springframework.web.bind.annotation.GetMapping;
+>>>>>>> Stashed changes
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,14 @@ public class UserController {
 
 	private final UserService userService;
 
+<<<<<<< Updated upstream
+=======
+	@GetMapping("/sendmail")
+	public String sendEmail() {
+		return "Email sent successfully";
+	}
+
+>>>>>>> Stashed changes
 	@PostMapping("/sign-up")
 	public ResponseEntity<Integer> signUp(HttpServletRequest request) {
 		// TODO : TEST
